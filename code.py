b9340707d2d82646137f873c7f10f70039be53cc
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2019-07-19 12:20:37 +0530

    entered random memes

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import telebot
 import pandas as pd
 import os
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,12 +12,13 @@ bot = telebot.TeleBot(HP_TOKEN) #balderdashBot
 df = pd.DataFrame(map(lambda X: map(str.strip,X.split('-')),open('spells.txt')),columns = ['Spells','Reactions'])
 print('Ready')
 
-def get_anim(spell):
-    file_name = '_'.join(spell.lower().split())+'.gif'
+def get_anim(spell = None, file_name = None, branch = 'master', file_path = 'gifs'):
+    if file_name == None:
+        file_name = '_'.join(spell.lower().split())+'.gif'
     user = "chttrjeankr"
     repository = "potter-verse"
-    branch = "master"
-    file_path = "gifs"
+    # branch = "master"
+    # file_path = "gifs"
     link = f"https://raw.githubusercontent.com/{user}/{repository}/{branch}/{file_path}/{file_name}"
     return link
 
@@ -33,24 +35,26 @@ def default_handler(message):
 
 @bot.message_handler(func=lambda message: True)
 def react(message):
-	# print(message)
-	# print()
-	try:
-		spell = df[df['Spells'].str.contains(message.text,case=False)]
-		reply = spell['Reactions'].to_list()[0]
-		try:
-			# anim = df[df['Spells'].str.contains(message.text,case=False)]['GIFs'].to_list()[0]
-			spell = spell['Spells'].to_list()[0]
-			anim = get_anim(spell)
-			print(anim)
-			msg = bot.send_document(message.from_user.id, anim)
-			print(msg)
-		except Exception as e:
-			print(e)
-			bot.send_message(message.from_user.id,"Let's assume there's something interesting in this message")
-	except:
-		reply = "I haven't learned that spell yet"
-	bot.reply_to(message, reply)
+    # print(message)
+    # print()
+    try:
+        spell = df[df['Spells'].str.contains(message.text,case=False)]
+        reply = spell['Reactions'].to_list()[0]
+        try:
+            spell = spell['Spells'].to_list()[0]
+            anim = get_anim(spell)
+            print(anim)
+            msg = bot.send_document(message.from_user.id, anim)
+            print(msg)
+        except Exception as e:
+            print(e)
+            bot.send_message(message.from_user.id,"Let's assume there's something interesting in this message")
+    except:
+        reply = "I haven't learned that spell yet"
+        meme = random.choice(os.listdir(os.getcwd()+'/funny'))
+        meme_url = get_anim(file_name = meme, file_path = 'funny')
+        bot.send_document(message.from_user.id, meme_url)
+    bot.reply_to(message, reply)
 
 
 bot.polling()
